005f60ef205bdca36a46484c50d806e8891db94b
N Srinivas <striker.dbz@hotmail.com>
2018-12-08 18:20:30 +0530

    Add files via upload

---
+++ LAPTOP_CHARGE_CONTROLLER/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 6/12/2018 1:34:53 AM
+ * Author : neutron
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
